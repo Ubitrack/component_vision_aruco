12ca73693e17cbc71839504e4eddca3a418c8339
Ulrich Eck <ulrich.eck@tum.de>
2018-11-25 11:28:17 +0100

    fix typo

--- conanfile.py
+++ conanfile.py
@@ -48,4 +48,4 @@ class UbitrackCoreConan(ConanFile):
         pass
 
     def package_id(self):
-        self.requires["ubitrack_vision"].full_package_mode()
+        self.info.requires["ubitrack_vision"].full_package_mode()
